8ab5e86c6e25fc3fa7899be167500f596d610cde
Ayoub Mesquiny <74786469+ayoubmesquiny@users.noreply.github.com>
2023-03-08 20:56:57 +0100

    first commit

--- script.py
+++ script.py
@@ -1,24 +1,117 @@
 import os
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
 from keras.utils import plot_model
-from keras.applications import InceptionV3
+from keras.applications import InceptionV3, VGG16, VGG19, EfficientNetB7, ResNet50, DenseNet121, Xception, InceptionResNetV2
 from keras.callbacks import EarlyStopping
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam, RMSprop
 from keras.regularizers import l2
 
 
+def load_base_model_InceptionResNetV2():
+    # Load the pre-trained model
+    base_model = InceptionResNetV2(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
 
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
 
-def load_base_model():
+    return base_model
+
+def load_base_model_Xception():
+    # Load the pre-trained model
+    base_model = Xception(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
+
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return base_model
+
+def load_base_model_DenseNet121():
+    # Load the pre-trained model
+    base_model = DenseNet121(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
+
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return base_model
+
+def load_base_model_InceptionV3():
     # Load the pre-trained model
     base_model = InceptionV3(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
+
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return base_model
+
+def load_base_model_ResNet50():
+    # Load the pre-trained model
+    base_model = ResNet50(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
+
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return base_model
+def load_base_model_VGG16():
+    # Load the pre-trained model
+    base_model = VGG16(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
+
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return base_model
+
+def load_base_model_VGG19():
+    # Load the pre-trained model
+    base_model = VGG19(
+        weights='imagenet', # Load pre-trained weights from the ImageNet dataset
+        include_top=False, # Set to False to exclude the top fully connected layers from the model
+        input_shape=(img_width, img_height, 3) # The input shape of the images
+    )
+
+    # Freeze the weights of the pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return base_model
+
+def load_base_model_EfficientNetB7():
+    # Load the pre-trained model
+    base_model = EfficientNetB7(
         weights='imagenet', # Load pre-trained weights from the ImageNet dataset
         include_top=False, # Set to False to exclude the top fully connected layers from the model
         input_shape=(img_width, img_height, 3) # The input shape of the images
@@ -124,7 +217,7 @@ def train_model(model, train_data, test_data, epochs, early_stop_patience):
 
     return history
 
-def plot_training_loss(history, save_dir=None):
+def plot_training_loss(history):
     # Plot the training and validation loss over the epochs
     plt.plot(history.history['loss'], label='Training Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
@@ -133,15 +226,11 @@ def plot_training_loss(history, save_dir=None):
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper left')
     plt.title('Training and Validation Loss')
-    plt.show()
 
     # Save the plot as an image
-    if save_dir:
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        plt.savefig(os.path.join(save_dir, 'loss_plot.png'))
+    plt.savefig('loss_plot.png')
 
-def plot_training_accuracy(history, save_dir=None):
+def plot_training_accuracy(history):
     # Plot the training and validation accuracy over the epochs
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
@@ -150,13 +239,23 @@ def plot_training_accuracy(history, save_dir=None):
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper left')
     plt.title('Training and Validation Accuracy')
-    plt.show()
 
     # Save the plot as an image
-    if save_dir:
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        plt.savefig(os.path.join(save_dir, 'accuracy_plot.png'))
+    plt.savefig('accuracy_plot.png')
+
+def plot_confusion_matrix(y_true, y_pred_classes):
+    # Compute the confusion matrix
+    cm = confusion_matrix(y_true, y_pred_classes)
+
+    # Plot the confusion matrix using seaborn
+    sns.heatmap(cm, annot=True, cmap='Blues')
+
+    # Set the axis labels
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+    
+    plt.savefig('confusion_matrix.png')
 
 
 
@@ -179,6 +278,15 @@ model = build_model(base_model)
 save_model_summary(model,'model_summary.txt')
 plot_model_architecture(model)
 model = compile_model(model)
+# Predict the classes for the validation data
+y_pred = model.predict(test_data)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+# Get the true classes for the validation data
+y_true = test_data.classes
+
+# Plot and save the confusion matrix
+plot_confusion_matrix(y_true, y_pred_classes)
 train_data, test_data =load_data(img_width, img_height, batch_size)
 history = train_model(model, train_data, test_data, epochs=100, early_stop_patience=10)
 plot_training_loss(history)
